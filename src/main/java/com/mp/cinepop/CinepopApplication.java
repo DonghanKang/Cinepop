c69229f2d41431c0a492447d4a0de0392e424919
DonghanKang <kdonghan2006@naver.com>
2021-12-22 21:38:08 +0900

    starter

---
+++ src/main/java/com/mp/cinepop/CinepopApplication.java
@@ -0,0 +1,13 @@
+package com.mp.cinepop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinepopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinepopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mp/cinepop/CinepopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mp.cinepop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinepopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
