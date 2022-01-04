ca1b2d9bd8df10afdc373b94fdd0d6c7c9d30af3
o3o122 <62046198+o3o122@users.noreply.github.com>
2022-01-04 23:27:55 +0900

    account /style_hl edit

--- src/main/java/com/mp/cinepop/account/model/AccountService.java
+++ src/main/java/com/mp/cinepop/account/model/AccountService.java
@@ -3,6 +3,15 @@ package com.mp.cinepop.account.model;
 import com.mp.cinepop.hash.model.hashVO;
 
 public interface AccountService {
+	//아이디 중복확인시 필요한 상수
+	public static final int EXIST_ID=1;  //해당 아이디가 이미 존재함
+	public static final int NON_EXIST_ID=2;  //해당 아이디가 존재하지 않음-사용가능
+	
+	//로그인 처리시 필요한 상수
+	public static final int LOGIN_OK=1;  //로그인 성공
+	public static final int DISAGREE_PWD=2; //비번 불일치
+	int USERID_NONE=3; //아이디 존재하지 않음
+	
 	public int insertAccount(AccountVO vo);
-	public int insertPw(hashVO hashvo);
+	public int insertHash(hashVO hashvo);
 }
