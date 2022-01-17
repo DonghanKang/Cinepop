72fecd98929a87fb80150b2f2f35f081dea13f6f
kimdowan0616 <ourbaby7@nate.com>
2022-01-18 01:39:03 +0900

    회원정보수정 pwd없이 로그인 추가
    account.xml > updateHash추가, pwd 관련 삭제
    accountController > hashing 추가
    accountModel > 관련기능 추가

--- src/main/java/com/mp/cinepop/account/model/AccountDAO.java
+++ src/main/java/com/mp/cinepop/account/model/AccountDAO.java
@@ -3,12 +3,16 @@ package com.mp.cinepop.account.model;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.mp.cinepop.common.SearchVO;
+import com.mp.cinepop.hash.model.hashVO;
 
 @Mapper
 public interface AccountDAO {
 	//public List<AccountVO> selectAll(SearchVO searchVO);
 	int selectTotalRecord(SearchVO searchVo);
 
+	public hashVO hashCheck(String salt);
+	public int updateHash(hashVO hashvo);
+	
 	public AccountVO selectByUserid(String userid);
 	public int updateAccount(AccountVO vo);
 	
