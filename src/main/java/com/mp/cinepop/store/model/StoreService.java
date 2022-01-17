5bd178908054a7ce054cc03fb0e14e1fd5ee6568
Donghan Kang <kdonghan2006@naver.com>
2022-01-18 02:40:57 +0900

    메인페이지 스토어 상품리스트 출력 by 동한

--- src/main/java/com/mp/cinepop/store/model/StoreService.java
+++ src/main/java/com/mp/cinepop/store/model/StoreService.java
@@ -10,6 +10,7 @@ public interface StoreService {
 	public int updatePd(StoreVO vo);
 	public StoreVO selectByPdNo(int pdNo);
 	public List<StoreVO> selectByCategory(String pctNo);
+	public List<StoreVO> selectFourByCategory(String pctNo);
 	public int deleteByPdNo(int pdNo);
 	public String getCategoryName(String pctNo);
 	public List<Map<String, Object>> selectCartByID(String id);
